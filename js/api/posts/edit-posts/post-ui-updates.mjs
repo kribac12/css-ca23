83666d6888dce3d9b9e639bc07c2e298a0eee395
kribac12 <89196018+kribac12@users.noreply.github.com>
2023-10-04 09:13:35 +0200

    saving update localStorage, remove inputs on click

--- js/api/posts/edit-posts/post-ui-updates.mjs
+++ js/api/posts/edit-posts/post-ui-updates.mjs
@@ -16,8 +16,18 @@ export async function handleEditFormSubmission(event, postId, titleElement, body
       mediaElement.src = updatedPost.media;
     }
 
-    // Notify user.
     console.log("Post updated successfully.");
+
+    // Local Storage update
+
+    let posts = JSON.parse(localStorage.getItem("posts")) || [];
+    const postIndex = posts.findIndex((p) => p.id === updatedPost.id);
+    if (postIndex !== -1) {
+      posts[postIndex] = updatedPost;
+      localStorage.setItem("posts", JSON.stringify(posts));
+    }
+
+    event.target.style.display = "none";
   } catch (error) {
     console.error("Error updating post:", error);
   }
